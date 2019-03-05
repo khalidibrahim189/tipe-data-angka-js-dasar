ff4848f0633f825559e7d0939fc5c8c550d5bd5d
khalidjambi <khalid.jambi18@gmail.com>
2019-03-05 14:31:54 +0700

    ketiga

---
+++ script.js
@@ -0,0 +1,26 @@
+/*1.memilih id wrap
+ 2.metode innerHTML = nulis konten
+*/
+
+// deklarasi Variable
+// var x = 1723645;
+// var tabungan = 120000;
+// document.getElementById("wrap").innerHTML = tabungan;
+
+//deklarasi variable
+
+// 1.nama dimulai dengan huruf atau $ atau _
+// 2.Nama boleh mengandung huruf,angka,underscore _ dan $ dolar
+// 3. Nama itu case sensitive
+// 4. Ada 'reserved words' yang tidak boleh dipakai
+var tabungan = 10;
+var Tabungan = 100;
+
+//tipe data string
+// var text = "sekolah koding";
+
+//tipe data angka/number
+var angka = 1000;
+var angka2 = 1000.123 + 0.2;
+
+document.getElementById("wrap").innerHTML = angka2;
